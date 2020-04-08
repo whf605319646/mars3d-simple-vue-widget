363cef8931ec065902830ed49c834eed05f6abe0
muyao <muyao1987@qq.com>
2020-04-08 20:29:43 +0800

    修改css

--- src/views/cesiumContainer.vue
+++ src/views/cesiumContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="cesiumContainer" class="cesium-container"></div>
+  <div id="cesiumContainer" class="mars3d-container"></div>
 </template>
 
 <script>
@@ -27,7 +27,7 @@ export default {
 </script>
 
 <style >
-.cesium-container {
+.mars3d-container {
   width: 100%;
   height: 100%;
 }
