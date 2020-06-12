91e340e51483e9dc7e46bb550588307d0dfdf2a3
muyao <muyao1987@qq.com>
2020-06-12 21:46:04 +0800

    新架构，封装地图为组件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,12 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  height: 100%;
-  background-color: burlywood;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+export default {
+  name: 'App'
+};
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="less">
 </style>
--- src/main.js
+++ src/main.js
@@ -1,24 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-//公共css
-import './assets/css/base.css';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
 //引入element
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 
-//引入fontawesome
-import 'font-awesome/css/font-awesome.min.css'
+import 'normalize.css'; 
+import './assets/style/common.less';
+ 
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App)
+}).$mount('#app');
